f44ac4a35ff23c445e51a32905843036d8b96164
CedricGAUTHIER <cedric.gauthier430@orange.fr>
2020-08-25 23:43:45 +0200

    shape mail content

--- server/app/controllers/contactController.js
+++ server/app/controllers/contactController.js
@@ -14,8 +14,8 @@ module.exports = {
             to: "test.projet.transport.de.fret@gmail.com", //exploitation@transportstdr.fr"
             subject: dataForm.subjectContact,
 
-            html: `Mail du contact : ${dataForm.mailContact} Nom du contact : ${dataForm.nameContact}  
-            Contenu du message : ${dataForm.commentContact}`
+            html: `<strong>Mail du contact :</strong> ${dataForm.mailContact} <br /> <strong>Nom du contact :</strong> ${dataForm.nameContact} <br /><br />  
+            <strong>Contenu du message :</strong> ${dataForm.commentContact}`
         };
 
         await contactDataMapper.sendMailContact(mail);
