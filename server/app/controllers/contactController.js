5a667c93f2e03d224c1e7338410e143287bcaa3b
EdwinGRANZOTTO <ed.granzotto@gmail.com>
2020-08-27 16:09:44 +0200

    Send HTML mail

--- server/app/controllers/contactController.js
+++ server/app/controllers/contactController.js
@@ -14,13 +14,15 @@ module.exports = {
             to: "test.projet.transport.de.fret@gmail.com", //exploitation@transportstdr.fr"
             subject: dataForm.subjectContact,
 
-            html: `<br /><strong>Mail du contact :</strong> ${dataForm.mailContact} <br /> <strong>Nom du contact :</strong> ${dataForm.nameContact} <br /><br />  
+            html: 
+            `<br /><strong>Mail du contact :</strong> ${dataForm.mailContact} <br /> <strong>Nom du contact :</strong> ${dataForm.nameContact} <br /><br />  
             <strong>Contenu du message :</strong> ${dataForm.commentContact}`
+
         };
 
         await contactDataMapper.sendMailContact(mail);
         
-        response.status(201).json(moment().format('LLLL'), " Le mail de contact a bien été envoyé ");
+        response.status(201).json(" Le mail de contact a bien été envoyé ");
 
     }
 }
--- server/app/controllers/quoteController.js
+++ server/app/controllers/quoteController.js
@@ -1,4 +1,5 @@
-const quoteDataMapper = require('../db/quoteDatamapper');
+const quoteDataMapper = require('../db/quoteDatamapper') ;
+const contactDataMapper = require('../db/contactDataMapper') ;
 const moment = require('moment') ;
 
 module.exports = {
@@ -27,10 +28,68 @@ module.exports = {
        
         await quoteDataMapper.createQuote(quoteBody);
         await quoteDataMapper.createSenderAddress(quoteBody);
-        await quoteDataMapper.createReceiverAddress(quoteBody); 
+        await quoteDataMapper.createReceiverAddress(quoteBody);      
         
-        console.log(moment().format('LLLL')," Creation : > > > Devis Complet Crée ");
-        response.status(201).json("Le formulaire de devis a bien été envoyé ");
+        dateNow = moment().format('LLLL') ;
+
+        const mail = {
+            
+            // <table cellspacing="0" cellpadding="0" border="0">
+            // <tbody><tr>
+            // <td>
+            // <a href="http://www.twitter.com/">
+            //     <img data-src="images/tw.gif" alt="Twitter" style="display: block;" width="38" height="38" border="0">
+            // </a>
+            // </td>
+            // <td style="font-size: 0; line-height: 0;" width="20"> </td>
+            // <td>
+            // <a href="http://www.twitter.com/">
+            //     <img data-src="images/fb.gif" alt="Facebook" style="display: block;" width="38" height="38" border="0">
+            // </a>
+            // </td>
+            // </tr>
+            // </tbody></table>
+            
+            from: "cgauthier.dev@gmail.com", //dataForm.mail,
+
+            to: "test.projet.transport.de.fret@gmail.com", //exploitation@transportstdr.fr"
+            subject: `Demande de devis du ${datenow} ` ,
+
+            html: 
+
+            `<table cellspacing="0" cellpadding="0" border="0">
+                <strong> Description du Colis </strong>
+                    <ul>
+                        <li> Nombre(s) de palette(s) : ${quoteBody.paletteNumberQuotePro} </li>
+                        <li> Poids : ${quoteBody.weightQuotePro} </li>
+                        <li> Longueur : ${quoteBody.weightQuotePro} </li>
+                    </ul>
+            
+            
+            <tbody><tr>
+            <td>
+            <strong>Adresse de Chargement</strong>
+            <a href="http://www.twitter.com/">
+                <img data-src="images/tw.gif" alt="Twitter" style="display: block;" width="38" height="38" border="0">
+            </a>
+            </td>
+            <td style="font-size: 0; line-height: 0;" width="20"> </td>
+            <td>
+            <strong>Adresse de Livraison<strong>
+            <a href="http://www.twitter.com/">
+                <img data-src="images/fb.gif" alt="Facebook" style="display: block;" width="38" height="38" border="0">
+            </a>
+            </td>
+            </tr>
+            </tbody></table>`
+
+            `<br /><strong>Desccription du colis :</strong> ${quoteBody.loadNumberQuotePro}<strong>Adresse de chargement :</strong> ${quoteBody.loadNumberQuotePro} ${quoteBody.loadTrackQuotePro} ${quoteBody.loadTrackQuotePro} <br /> <strong>Nom du contact :</strong> ${quoteBody.nameContact} <br /><br />  
+            <strong>Contenu du message :</strong> ${quoteBody.commentContact}`
+        };
+
+        await contactDataMapper.sendMailContact(mail);
         
+        console.log(moment().format('LLLL')," Creation : Devis Complet ");
+        response.status(201).json("Le formulaire de devis a bien été transmis par Mail ");
     }
 }
