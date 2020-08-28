8f519aa26a9b597b4d8df4dcff4d95305b11aebb
EdwinGRANZOTTO <ed.granzotto@gmail.com>
2020-08-28 12:07:12 +0200

    Add Try Catch on DataMapper

--- server/app/db/conInscDataMapper.js
+++ server/app/db/conInscDataMapper.js
@@ -3,12 +3,40 @@ const client = require('./db');
 module.exports = {
 
     findUser: async (body) => {
-        const user = await client.query('SELECT * FROM "user" WHERE "mail"= $1', [body.mailLogin]);
-        return user.rows[0];
+
+        try {
+        
+            const user = await client.query('SELECT * FROM "user" WHERE "mail"= $1', [body.mailLogin]);
+            return user.rows[0];
+
+        } catch (error) {
+            
+            console.trace(moment().format('LLLL'), error) ;
+            response.status(500).send(error) ;
+
+        }
     },
 
     addBillAddress : async (bodyAddress) => {
         
+        try {
+            
+            await client.query('INSERT INTO "bill_address" ("main", "complement","postal_code","city") VALUES ($1,$2,$3,$4) RETURNING "id"',
+                [
+                 `${bodyAddress.billNumber} ${bodyAddress.billTrack} ${bodyAddress.billStreet}`,
+                 bodyAddress.billComplement ,
+                 bodyAddress.postalCode ,
+                 bodyAddress.city
+                ] 
+            );
+
+        } catch (error) {
+            
+            console.trace(moment().format('LLLL'), error) ;
+            response.status(500).send(error) ;
+
+        }
+
         const newBillAddress = await client.query(
             'INSERT INTO "bill_address" ("main", "complement","postal_code","city") VALUES ($1,$2,$3,$4) RETURNING "id"',
             [
@@ -25,32 +53,51 @@ module.exports = {
     },
 
     addUserPro : async (result,billAddressId) => {
-        await client.query(
-            'INSERT INTO "user" ("name", "password","mail","phone","type","siret","bill_address_id")VALUES ($1,$2,$3,$4,$5,$6,$7)',
-            [
-            result.societe ,
-            result.passwordSignUpPro ,
-            result.mailSignUpPro ,
-            result.phoneSignUpPro ,
-            "pro" ,
-            result.siret,
-            billAddressId.id
-            ] 
-        )
+
+        try {
+        
+            await client.query('INSERT INTO "user" ("name", "password","mail","phone","type","siret","bill_address_id")VALUES ($1,$2,$3,$4,$5,$6,$7)',
+                [
+                result.societe ,
+                result.passwordSignUpPro ,
+                result.mailSignUpPro ,
+                result.phoneSignUpPro ,
+                "pro" ,
+                result.siret,
+                billAddressId.id
+                ] 
+            )
+
+        } catch (error) {
+        
+            console.trace(moment().format('LLLL'), error) ;
+            response.status(500).send(error) ;
+
+        }
     },
 
     addUserPart : async (result,billAddressId) => {
-        await client.query(
-            'INSERT INTO "user" ("name", "password","mail","phone","type","bill_address_id")VALUES ($1,$2,$3,$4,$5,$6)',
-            [
-            `${result.firstNameSignUpPart}  ${result.nameSignUpPart}` ,
-            result.passwordSignUpPart ,
-            result.mailSignUpPart ,
-            result.phoneSignUpPart ,
-            "part" ,
-            billAddressId.id
-            ] 
-        )
-    }    
 
+        try {
+        
+            await client.query(
+
+                'INSERT INTO "user" ("name", "password","mail","phone","type","bill_address_id")VALUES ($1,$2,$3,$4,$5,$6)',
+                [
+                `${result.firstNameSignUpPart}  ${result.nameSignUpPart}` ,
+                result.passwordSignUpPart ,
+                result.mailSignUpPart ,
+                result.phoneSignUpPart ,
+                "part" ,
+                billAddressId.id
+                ] 
+            )
+
+        } catch (error) {
+        
+            console.trace(moment().format('LLLL'), error) ;
+            response.status(500).send(error) ;
+        
+        }    
+    }
 }
--- server/app/db/contactDataMapper.js
+++ server/app/db/contactDataMapper.js
@@ -6,25 +6,33 @@ module.exports = {
     
     sendMailContact: async(mail) => {
 
-        const smtpTransport = mailer.createTransport({
-            service: "Gmail",
-            auth: {
-                user: "test.projet.transport.de.fret@gmail.com",
-                pass: "13_06_38_TDR"
-            }
+        try {
+        
+            const smtpTransport = mailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: "test.projet.transport.de.fret@gmail.com",
+                    pass: "13_06_38_TDR"
+                }
+                });
+    
+            await smtpTransport.sendMail(mail, function(error,response){
+                
+                if(error){
+                    console.log(moment().format('LLLL')," Erreur lors de l'envoi du mail");
+                    console.log("Descriptif erreur Echec envoi du mail", error );
+                } else {
+                    console.log(moment().format('LLLL'),` Mail envoyé avec succès pour le ${mail.message} `)
+                    
+                }
+                smtpTransport.close();
+    
             });
 
-        await smtpTransport.sendMail(mail, function(error,response){
-            
-            if(error){
-                console.log(moment().format('LLLL')," Erreur lors de l'envoi du mail");
-                console.log("Descriptif erreur Echec envoi du mail", error );
-            } else {
-                console.log(moment().format('LLLL'),` Mail envoyé avec succès pour le ${mail.message} `)
-                
-            }
-            smtpTransport.close();
-
-        });
+        } catch (error) {
+        
+            console.trace(moment().format('LLLL'), error) ;
+            response.status(500).send(error) ;
+        }
     }
 }
