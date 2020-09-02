618a6305296de78ca0d0f43527dcba130eed83ae
Marwan-msr <marwanmansour733@gmail.com>
2020-09-03 00:30:55 +0200

    tdr 3

--- client/src/components/Aacueil/index.js
+++ client/src/components/Aacueil/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './style.scss';
 import logo from '../../assets/images/IMG_2009.png'
+import mar from '../../assets/images/mar.png'
 
 const Aacueil = () => {
 return (
     
-   <div>
-       <div className="logoAccueil">
-<img  style={{width:300,height:300}} alt="salut" src={logo}/>
-</div>
-<h1>transport TDR</h1>
-
+    <div>
+       <h1 className="tdr">transport TDR</h1>
+   <base href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/"/>
+   <div id="slider">
+   <figure>
+   <img className="oma" style={{width:1664, height:1000}} src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/31230570_1782337928741576_7777859956836925440_n.jpg?_nc_cat=103&_nc_sid=e3f864&_nc_ohc=FrOOEfiEha8AX_QtSOM&_nc_ht=scontent-cdt1-1.xx&oh=c703809464909250427f7915b6d684e9&oe=5F76260C" />
+   <img  className="oma" style={{width:1664, height:1000}} src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/21432848_1703113769997326_8621590206268566747_n.jpg?_nc_cat=105&_nc_sid=8bfeb9&_nc_ohc=pbZv-J3W2ocAX-vFR_w&_nc_ht=scontent-cdt1-1.xx&oh=5dcc4de3dc833a64b4023998b75a78e6&oe=5F7498D3" alt/>
+   <img className="oma" style={{width:1664, height:1000}}  src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/70266638_2067317886910244_1509135621006819328_o.jpg?_nc_cat=106&_nc_sid=e3f864&_nc_ohc=AscP3zrI07IAX_zHevY&_nc_ht=scontent-cdt1-1.xx&oh=d68278bad6f96cc24d8609bee112f9e5&oe=5F73B97E" alt/>
+   <img  className="oma" style={{width:1664, height:1000}}  src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/35737609_1805740616401307_3424535982408466432_n.jpg?_nc_cat=107&_nc_sid=8bfeb9&_nc_ohc=OwY3FxjlgFwAX8QeCRn&_nc_ht=scontent-cdg2-1.xx&oh=4b5235bd8e211baebd0e1f38e8cdfa62&oe=5F75B22A" alt/>
+   <img src="austin-fireworks.jpg" alt/>
+   </figure>
    </div>
-  
+   </div>
 )
 }
 
