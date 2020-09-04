3e961a89904798c809b79735386200066544c11b
gca703 <gregory.caruso703@gmail.com>
2020-09-04 09:36:53 +0200

    working on css

--- client/src/components/Mots/index.js
+++ client/src/components/Mots/index.js
@@ -9,7 +9,7 @@ return (
 <div className="split leftMots">
     <div className="textMots">
       <h2 className="h2Mots">En quelques mots</h2>
-    <p className="pMots">Transport Tdr est une entreprise française fondée en 1980, spécialisée dans le groupage et la distribution de fret à haute valeur ajoutée. Nos entrepôts sécurisés sont idéalement situés aux croisements des axes autoroutiers et nous permettent de proposer tout une gamme de prestations logistiques.</p>
+    <p className="pMots">Transports TDR est une entreprise française fondée en 1980, spécialisée dans le groupage et la distribution de fret à haute valeur ajoutée. Nos entrepôts sécurisés sont idéalement situés aux croisements des axes autoroutiers et nous permettent de proposer tout une gamme de prestations logistiques.</p>
     </div>
     
   
